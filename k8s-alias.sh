9f851ff4a4b9f996dc8f5d4d7579bf40a2a95e4c
Murat Akpınar <112278587+murat-akpinar@users.noreply.github.com>
2023-11-01 20:59:06 +0300

    Update k8s-alias.sh

--- k8s-alias.sh
+++ k8s-alias.sh
@@ -1,29 +1,25 @@
 #!/bin/bash
 
+cat <<EOL >> ~/.bashrc
+alias k='kubectl'
+alias ks='kubectl -n kube-system'
+alias kdesc='kubectl describe'
+alias kcf='kubectl create -f'
+alias kaf='kubectl apply -f'
+alias kgn='kubectl get nodes'
+alias kgp='kubectl get pods'
+alias kall='kubectl get pods -A'
+alias kname='kubectl get pods -n'
+alias kgpa='kubectl get pods --all-namespaces'
+alias kgs='kubectl get services'
+alias kgd='kubectl get deployments'
+alias kd='kubectl delete'
+alias kdp='kubectl delete pod'
+alias kds='kubectl delete service'
+alias kdd='kubectl delete deployment'
+alias kdn='kubectl delete namespace'
+alias kep='kubectl edit pods'
+alias kdesp='kubectl describe pods'
+alias kexp='kubectl explain'
+EOL
 
-echo "alias k='kubectl'" >> ~/.bashrc
-echo "alias ks='kubectl -n kube-system'" >> ~/.bashrc
-echo "alias kdesc='kubectl describe'" >> ~/.bashrc
-
-echo "alias kcf='kubectl create -f'" >> ~/.bashrc
-echo "alias kaf='kubectl apply -f'" >> ~/.bashrc
-
-echo "alias kgn='kubectl get nodes'" >> ~/.bashrc
-echo "alias kgp='kubectl get pods'" >> ~/.bashrc
-echo "alias kall='kubectl get pods -A'" >> ~/.bashrc
-echo "alias kname='kubectl get pods -n'" >> ~/.bashrc
-echo "alias kgpa='kubectl get pods --all-namespaces'" >> ~/.bashrc
-echo "alias kgs='kubectl get services'" >> ~/.bashrc
-echo "alias kgd='kubectl get deployments'" >> ~/.bashrc
-
-echo "alias kd='kubectl delete'" >> ~/.bashrc
-echo "alias kdp='kubectl delete pod'" >> ~/.bashrc
-echo "alias kds='kubectl delete service'" >> ~/.bashrc
-echo "alias kdd='kubectl delete deployment'" >> ~/.bashrc
-echo "alias kdn='kubectl delete namespace'" >> ~/.bashrc
-
-echo "alias kep='kubectl edit pods'" >> ~/.bashrc
-echo "alias kdesp='kubectl describe pods'" >> ~/.bashrc
-echo "alias kexp='kubectl explain'" >> ~/.bashrc
-
-exec bash
